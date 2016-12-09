b153b62b43748b21a57e1e185e811da823c022ef
rbross <rbross@dyn.com>
2016-12-09 11:06:42 -0500

    Add sections to get system temp and fans from IPMI

---
+++ ipmifans.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Note that the following entry was created in the sudoers file using visudo to 
+# allow any user in the sudo group to execute ipmi-sensors without entering a PW:
+# # Allow any user in the sudo group to run ipmi-sensors w/o pw
+# %sudo ALL=(ALL) NOPASSWD: /usr/sbin/ipmi-sensors
+
+sudo ipmi-sensors | grep "Fan$1" | cut -d'|' -f4 | cut -d' ' -f2
